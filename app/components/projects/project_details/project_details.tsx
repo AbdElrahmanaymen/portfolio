0c8dc81b60bbe78178df0370155367c718bb04fc
Abd-Elrahman Aymen <Abdelrahmanaymen@outlook.com>
2024-01-03 05:06:14 +0800

    Update 1
    -Implementing Project Details as a Component
    -Implementing Buttons for app store/play store
    -Implementing Props for project details component

---
+++ app/components/projects/project_details/project_details.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+import AppStoreButton from "../../buttons/app_store_btn";
+import PlayStoreButton from "../../buttons/play_store_btn";
+import { ProjectDetailsProps } from "../../../types/types"
+
+const ProjectDetailsComponent: React.FC<ProjectDetailsProps> = ({
+  appName,
+  appPhoto,
+  appDescription,
+  appTags,
+  appStoreLink,
+  playStoreLink,
+  previewVideo,
+}) => {
+  return (
+    <div className="flex flex-col w-full h-screen lg:flex-row py-10">
+      <div className="grid flex-grow place-items-center">
+        <Image
+          alt=""
+          width={0}
+          height={0}
+          sizes="100vw"
+          src={appPhoto}
+          className="object-contain w-fit"
+        />
+      </div>
+      <div className="grid flex-grow place-items-center">
+        <article className="prose">
+          <h1>{appName}</h1>
+          {appTags.map((value, index) => (
+            <div className="badge badge-primary mr-1" key={index}>
+              {value}
+            </div>
+          ))}
+          <p>{appDescription}</p>
+          <div className="flex gap-5 items-center">
+            {appStoreLink.length > 0 ? <AppStoreButton link={appStoreLink} /> : null} 
+            {playStoreLink.length > 0 ? <PlayStoreButton link={playStoreLink} /> : null}
+            {previewVideo.length > 0 ? <button className="btn btn-outline">Watch Video</button> : null}       
+          </div>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetailsComponent;
---
+++ app/types/types.ts
@@ -0,0 +1,13 @@
+export interface ProjectDetailsProps {
+  appName: string;
+  appPhoto: string;
+  appDescription: string;
+  appTags: string[];
+  appStoreLink: string;
+  playStoreLink: string;
+  previewVideo: string;
+}
+
+export interface AppProps {
+  link: string;
+}
