1d14c77ab41457b6eb8da0874f0774f01295ee33
Abd-Elrahman Aymen <Abdelrahmanaymen@outlook.com>
2024-01-06 02:52:59 +0800

    Update Navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,20 +1,18 @@
-"use client"
-
-import React from 'react'
+"use client";
+import React from "react";
 
 const Navbar = () => {
-
-  const scrollToSection = (sectionId:string) => {
+  const scrollToSection = (sectionId: string) => {
     const element = document.getElementById(sectionId);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
     <div className='navbar bg-base-100'>
     <div className='flex-1'><a className='btn btn-ghost text-xl'>Abdelrahman</a></div>
-    <div className="flex-none">
+    <div className="flex-none hidden lg:block">
       <ul className="menu menu-horizontal px-1">
         <li onClick={() => scrollToSection("home")}><a>Home</a></li>
         <li onClick={() => scrollToSection("testimonials")}><a>Testimonials</a></li>
@@ -25,7 +23,7 @@ const Navbar = () => {
       </ul>
     </div>
   </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
