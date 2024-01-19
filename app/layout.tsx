731e28db9875b68398088bb8ece22de889c998b2
Abd-Elrahman Aymen <Abdelrahmanaymen@outlook.com>
2024-01-19 19:34:20 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     siteName: "Abdelrahman Portfolio",
     images: [
       {
-        url: "http://abdelrahmanaymen.tech/og.jpg",
+        url: "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/abdelrahmanaymen.tech/Abdelrahman%20Portfolio/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2Fa16aea57-f8c4-4df6-bf53-7210e181033d.jpg%3Ftoken%3DNRyHOnn4vhj28BUyQ37RIp4Iwl0tuSpZlFYtasntcxc%26height%3D401%26width%3D750%26expires%3D33241663994/og.png",
       },
     ],
   },
