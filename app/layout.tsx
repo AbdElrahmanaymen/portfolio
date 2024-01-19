e0d1f4dde648c1ee9167584db6a1c67c0edacf5f
Abd-Elrahman Aymen <Abdelrahmanaymen@outlook.com>
2024-01-19 19:49:14 +0800

    Update OG Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Abdelrahman Portfolio",
   description:
-    "Explore my world of robotics and programming innovation, where passion drives impactful solutions.",
+    "Embark on a captivating journey into my world of robotics and programming innovation. Explore a diverse portfolio where passion converges with technical expertise, giving rise to impactful solutions that redefine the possibilities of technology. Join me on this exciting exploration of cutting-edge projects and discover the transformative potential of my work in shaping the future of robotics and programming.",
   icons: {
     icon: [
       {
@@ -22,11 +22,11 @@ export const metadata: Metadata = {
     url: "http://abdelrahmanaymen.tech",
     title: "Abdelrahman Portfolio",
     description:
-      "Explore my world of robotics and programming innovation, where passion drives impactful solutions.",
+      "Embark on a captivating journey into my world of robotics and programming innovation. Explore a diverse portfolio where passion converges with technical expertise, giving rise to impactful solutions that redefine the possibilities of technology. Join me on this exciting exploration of cutting-edge projects and discover the transformative potential of my work in shaping the future of robotics and programming.",
     siteName: "Abdelrahman Portfolio",
     images: [
       {
-        url: "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/abdelrahmanaymen.tech/Abdelrahman%20Portfolio/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2Fa16aea57-f8c4-4df6-bf53-7210e181033d.jpg%3Ftoken%3DNRyHOnn4vhj28BUyQ37RIp4Iwl0tuSpZlFYtasntcxc%26height%3D401%26width%3D750%26expires%3D33241663994/og.png",
+        url: "http://abdelrahmanaymen.tech/og.png",
       },
     ],
   },
